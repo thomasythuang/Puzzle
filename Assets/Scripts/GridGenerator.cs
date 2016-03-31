394348527681fb8d0cbffe2e3fd5de212f0a21f0
Thomas Huang <thomas.yt.huang@gmail.com>
2016-03-31 18:35:53 -0500

    change grids to resize with screen size

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -5,54 +5,56 @@ using System.Collections;
 public class GridGenerator : MonoBehaviour {
 
     public GameObject tilePrefab;
-    public int xBound;
-    public int yBound;
+    public int xlen;
+    public int ylen;
+    private float xmin;
+    private float ymin;
     public bool run;
     public bool savePrefab;
     public string fileName;
     private string filePath;
     private GameObject grid;
 
-    // Use this for initialization
+    // Initialization
     void Start () {
-        filePath = "Assets/Resources/Prefabs/" + fileName + ".prefab";
         if (run)
         {
+            xmin = (float)(xlen - 1) / -2;
+            ymin = (float)(ylen - 1) / -2;
             grid = new GameObject();
             CreateTiles();
         }
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
 	}
 
     private void CreateTiles()
     {
-        int xlen = 2 * xBound + 1;
-        int ylen = 2 * yBound + 1;
+        // Init temp array to hold our tile objects
         GameObject[][] tileArray = new GameObject[xlen][];
         for (int i = 0; i < xlen; i++)
         {
             tileArray[i] = new GameObject[ylen];
         }
 
-        for (int y = yBound * -1; y <= yBound; y++)
-        {
-            for (int x = xBound * -1; x <= xBound; x++)
-            {
-                GameObject tile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity) as GameObject;
-                tile.transform.parent = grid.transform;
-                tileArray[x + xBound][y + yBound] = tile;
-            }
-        }
-
+        // Instantiate the grid of tiles
         for (int y = 0; y < ylen; y++)
         {
             for (int x = 0; x < xlen; x++)
             {
-                //Debug.Log(tileArray[x][y].transform.position.x.ToString() + ", " + tileArray[x][y].transform.position.y.ToString());
+                GameObject tile = Instantiate(tilePrefab, new Vector3(x+xmin, y+ymin), Quaternion.identity) as GameObject;
+                tile.transform.parent = grid.transform;
+                tileArray[x][y] = tile;
+            }
+        }
+
+        // For each tile, set references to its neighboring tiles
+        for (int y = 0; y < ylen; y++)
+        {
+            for (int x = 0; x < xlen; x++)
+            {
                 TileScript tileScript = tileArray[x][y].GetComponent<TileScript>();
                 if (x > 0)
                 {
@@ -73,8 +75,16 @@ public class GridGenerator : MonoBehaviour {
             }
         }
 
+        // Attach GridScript to the instantiated grid
+        grid.AddComponent<GridScript>();
+        GridScript gridScript = grid.GetComponent<GridScript>();
+        gridScript.xlen = xlen;
+        gridScript.ylen = ylen;
+
+        // Save this as a prefab if specified
         if (savePrefab)
         {
+            filePath = "Assets/Resources/Prefabs/" + fileName + ".prefab";
             PrefabUtility.CreatePrefab(filePath, grid);
         }
     }
---
+++ Assets/Scripts/GridScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridScript : MonoBehaviour {
+
+    public float xMax; // This is a ratio in relation to the total screen size: (0,1)
+    public float yMax;
+    public int xlen;
+    public int ylen;
+
+    // Use this for initialization
+    void Start()
+    {
+        AdjustScreen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void AdjustScreen()
+    {
+        float width = Camera.main.orthographicSize * 2.0f * Screen.width / Screen.height;
+        float height = Camera.main.orthographicSize * 2.0f;
+
+        float xRatio = xlen / width;
+        float yRatio = ylen / height;
+
+        float scale = Mathf.Min(xMax / xRatio, yMax / yRatio);
+
+        Debug.Log("xRatio: " + xRatio.ToString());
+        Debug.Log("yRatio: " + yRatio.ToString());
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+}
